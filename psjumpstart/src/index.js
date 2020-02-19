fdd18f1d22711407c7b559ccf62b59926f05828d
Drew Paschal <drew.paschal@publicissapient.com>
2020-02-19 14:08:15 -0500

    instant refresh through webpack module

--- psjumpstart/src/index.js
+++ psjumpstart/src/index.js
@@ -4,8 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
 
+let render = () => {
+  ReactDOM.render(<App />, rootEl);
+};
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    setTimeout(render, 2000);
+  });
+}
+
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
