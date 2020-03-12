ed044ada66c246e084ce789b8b721982245819b4
drepasch2 <drewjpaschal@gmail.com>
2020-03-12 13:31:31 -0400

    adding text input component

---
+++ psjumpstart/src/app/common/form/TextInput.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TextInput = ({
+  input,
+  width,
+  type,
+  placeholder,
+  meta: { touched, error }
+}) => {
+  return (
+    <Form.Field error={touched && !!error}>
+      <input {...input} placeholder={placeholder} type={type} />
+      {touched && error && (
+        <Label basic color="red">
+          {error}
+        </Label>
+      )}
+    </Form.Field>
+  );
+};
+
+export default TextInput;
